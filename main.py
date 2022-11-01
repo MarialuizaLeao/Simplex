480a2bfc9830cef116853a0b4cda4d85103f58bf
MarialuizaLeao <maria_luiza1598@hotmail.com>
2022-10-31 22:20:18 -0300

    Função pivot

---
+++ main.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+class Tableu:
+
+    def __init__(self):
+        self.c = []
+        self.A = []
+        self.b = []
+
+# Queremos saber a coluna que vai entrar na base e que linha dessa coluna vai ser pivoteada, ademais, checamos para verificar a pl não é ilimitada
+def findPivot(A, b, c):
+    N = A.shape[0]
+    M = A.shape[1]
+    pivotColum = -1000
+    pivotLine = -1000
+    ilimitada = False
+    for i in range(0, M):  # Passando por cada coluna
+        if(c[i] < 0):  # Esse valor pode ser pivoteado
+            if(np.all(A[:, i] <= 0)):  # Se todas as linhas da coluna i são negativos ou iguais à zero
+                ilimitada = True
+                break
+            minValue = 1000
+            for j in range(0, N):  # Passando por cada linha
+                if(A[j][i] > 0):
+                    valueLine = b[j] / A[j][i]
+                if(valueLine < minValue):
+                    minValue = valueLine
+            pivotColum = i
+            pivotLine = j
+            break
+
+    return pivotColum, pivotLine, ilimitada
+            
