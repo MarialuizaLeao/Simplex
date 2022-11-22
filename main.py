a4bf3f804cd58c2a32449f301c4fc8fce7847277
MarialuizaLeao <maria_luiza1598@hotmail.com>
2022-11-22 19:02:08 -0300

    finding certificate

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ class Tableau:
         self.c = []
         self.A = []
         self.b = []
+        self.certificate = []
+        self.vero = []
         self.optimalValue = 0
         self.baseColumns = []
         self.dimensions = (0,0)
@@ -46,20 +48,25 @@ class Tableau:
             if (self.A[i][self.baseColumns[i]] != 0):  # If the pivot for this variable is diferent than 0
                 value = self.A[i][self.baseColumns[i]].copy()
                 self.A[i, :] /= value  # Divide the entire line by the pivot value, so now pivot = 1
-                self.b[i] /= value    
+                self.b[i] /= value
+                self.vero[i, :] /= value    
             for j in range(0, self.dimensions[0]):  # For each line
                 if (i != j):  # If it's not the base variable line
                     value = self.A[j][self.baseColumns[i]].copy()
                     self.A[j, :] -= value * self.A[i, :]  # So we have the rest of the pivot colums = 0
                     self.b[j] -= value * self.b[i]
+                    self.vero[j, :] -= value * self.vero[i, :]
             value = self.c[self.baseColumns[i]]
             self.c -= value * self.A[i, :]  # So now the variable is a real base (respective c = 0)
             self.optimalValue -= value * self.b[i]
+            self.certificate -= value * self.vero[i, :]
         vfunc = np.vectorize(zero)  # Round all the zeros according to the precision 
         self.A = vfunc(self.A)
         self.c = vfunc(self.c)
         self.b = vfunc(self.b)
         self.optimalValue = vfunc(self.optimalValue)
+        self.certificate = vfunc(self.certificate)
+        self.vero = vfunc(self.vero)
 
     # Find the solution in the tableau
     def findX(self):
@@ -70,7 +77,7 @@ class Tableau:
 
         return x
 
-def simplex(restrictions, bVector, optimalVector, baseVariables):
+def simplex(restrictions, bVector, optimalVector, baseVariables, vero):
     tableau = Tableau()  # Initiate the tableu with the receving values
     tableau.A = restrictions.copy()
     tableau.b = bVector.copy()
@@ -78,13 +85,15 @@ def simplex(restrictions, bVector, optimalVector, baseVariables):
     tableau.baseColumns = baseVariables.copy()
     tableau.dimensions = (restrictions.shape[0], restrictions.shape[1])
     tableau.optimalValue = 0
+    tableau.certificate = np.zeros(tableau.dimensions[0])
+    tableau.vero = vero
     tableau.canonizeTableau()  # Make sure that all the bases columns really have pivots
     while(np.any(tableau.c < 0)):  # While c still have negative values
         pivotColumn, pivotLine, ilimitada = tableau.findPivot()  # Find the variable that will enter the base
         if (ilimitada):  # If we find that the problem is unbounded
             tableau.plClassification = 'ilimitada'
             solution = tableau.findX()  # Find the solution that we have in the moment
-            return tableau.optimalValue, solution, tableau.plClassification, tableau.baseColumns
+            return tableau.certificate, tableau.optimalValue, solution, tableau.plClassification, tableau.baseColumns
         tableau.baseColumns[pivotLine] = pivotColumn  # Add the new pivot column found to the bases list
         tableau.canonizeTableau()  # Put the tableau in canonical form for the new base value
     solution = tableau.findX()  # Find the tableau's solution
@@ -93,7 +102,7 @@ def simplex(restrictions, bVector, optimalVector, baseVariables):
     else:
         tableau.plClassification = 'otima'
         
-    return tableau.optimalValue, solution, tableau.plClassification, tableau.baseColumns
+    return tableau.certificate, tableau.optimalValue, solution, tableau.plClassification, tableau.baseColumns
 
 # Return the auxiliar pl
 def auxiliarPl(originalA, originalB):
@@ -122,6 +131,7 @@ for i in range(0, N):
     restrictionsInput.append(input().split())
 restrictionsInput = np.array(restrictionsInput, dtype = float)
 compensatingVariables = np.eye(N, dtype = float)
+veroMatrix = np.eye(N, dtype = float)
 
 baseInput = np.array(restrictionsInput[:, -1])  # b vector
 
@@ -141,13 +151,15 @@ if(np.any(baseInput < 0)):  # Check to see if any b value is negative
         if(baseInput[i] < 0):
             baseInput[i] *= -1
             restrictionsInput[i][:] *= -1
+            veroMatrix[i][:] *= -1
 
 auxiliarA, auxiliarB, auxiliarC, auxiliarBaseVariables = auxiliarPl(restrictionsInput, baseInput)  # Create the auliar matrix for the problem
 
-auxiliarOptimalValue, auxiliarSolution, auxiliarPlClassification, auxiliarFinalBaseVariables = simplex(auxiliarA, auxiliarB, auxiliarC, auxiliarBaseVariables)  # Solve the auxiliar problem
+auxiliarCertificate, auxiliarOptimalValue, auxiliarSolution, auxiliarPlClassification, auxiliarFinalBaseVariables = simplex(auxiliarA, auxiliarB, auxiliarC, auxiliarBaseVariables, veroMatrix.copy())  # Solve the auxiliar problem
 
 if(auxiliarOptimalValue < 0):  # If auxiliar's optimal value is negative, then the problem is unsolvable
     print(auxiliarPlClassification)
+    printArray(auxiliarCertificate)
 else:
     if(bNegativo):  # If the original problem had negative b values, we'll use the base variables found by the auxiliar matrix
         auxiliarFinalBaseVariables.sort()
@@ -156,7 +168,7 @@ else:
             if(auxiliarFinalBaseVariables[i] > M + N):
                 auxiliarFinalBaseVariables[i] = auxiliarFinalBaseVariables[i] - (M + N)
         baseVariablesInput = auxiliarFinalBaseVariables
-    FinalOptimalValue, FinalSolution, FinalPlClassification, FinalFinalBaseVariables = simplex(restrictionsInput, baseInput, optimalVectorInput, baseVariablesInput)  # Solve the original problem
+    FinalCertificate, FinalOptimalValue, FinalSolution, FinalPlClassification, FinalFinalBaseVariables = simplex(restrictionsInput, baseInput, optimalVectorInput, baseVariablesInput, veroMatrix)  # Solve the original problem
     vfunc = np.vectorize(zero)
     solution = vfunc(FinalSolution)
     optimalValue = vfunc(FinalOptimalValue)
@@ -166,4 +178,5 @@ else:
     if(FinalPlClassification == 'otima'):  # In this case, print optimal value and result
         print('{:.7f}'.format(FinalOptimalValue))
         printArray(FinalSolution)
+        printArray(FinalCertificate)
         
